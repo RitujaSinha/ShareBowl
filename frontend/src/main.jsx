df083460db8e138ac3dc6a77f74e836118a22b9c
Shivani <shivanikumari9979@gmail.com>
2026-05-21 18:33:17 +0530

    Sign-up pages added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,18 @@
 import './index.css'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from './pages/Home'
-import Signup from './pages/Signup'
+import Home from './pages/Home.jsx'
+import Signup from './pages/Signup.jsx'
+import OrganizationSignup from './pages/organisation/OrganizationSignup.jsx'
+import DonorSignup from './pages/donor/DonorSignup.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route  path="/" element={<Home/>} />
       <Route path = "/signup" element = {<Signup/>} />
+      <Route path="/signup/donorsignup" element={<DonorSignup />} />
+      <Route path="/signup/organizationsignup" element={<OrganizationSignup />} />
     </Routes>
   </BrowserRouter>
 )
