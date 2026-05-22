f4a0aefdfaf7336826893da22a916b53f988fbdc
Shivani <shivanikumari9979@gmail.com>
2026-05-22 20:04:23 +0530

    Login page added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './pages/Home.jsx'
 import Signup from './pages/Signup.jsx'
+import Login from './pages/Login.jsx'
 import OrganizationSignup from './pages/organisation/OrganizationSignup.jsx'
 import DonorSignup from './pages/donor/DonorSignup.jsx'
 import DonorDashboard from './pages/donor//DonorDashboard'
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')).render(
 
       <Route  path="/" element={<Home/>} />
       <Route path = "/signup" element = {<Signup/>} />
+      <Route path= "/login" element = {<Login/>} />
       <Route path="/signup/donorsignup" element={<DonorSignup />} />
       <Route path="/signup/organizationsignup" element={<OrganizationSignup />} />
       <Route path="/donor" element={<DonorDashboard />} />
