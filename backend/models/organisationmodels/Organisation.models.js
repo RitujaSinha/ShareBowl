e4a7a8fca2dab5b16bd00aeb537127f6593b543e
Shivani <shivanikumari9979@gmail.com>
2026-05-22 20:57:05 +0530

    Organisation model added

---
+++ backend/models/organisationmodels/Organisation.models.js
@@ -0,0 +1,91 @@
+import mongoose from "mongoose"
+import { Schema } from "mongoose"
+
+const organisationSchema = new Schema({
+    organisationName:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+
+    organisationID:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true
+    },
+
+    organisationOwner:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+    },
+
+    isEmailVerified:{
+        type: Boolean,
+        default: false
+    },
+    isAdminVerified: {
+        type: Boolean,
+        default: false
+    },
+
+    phone:{
+        type:Number,
+        immutable:true,
+        maxLength:10,
+        minLength:10,
+    },
+
+    street:{
+        type:String,
+        lowercase:true,
+        required:true,
+        trim:true,
+    },
+
+    city:{
+        type:String,
+        lowercase:true,
+        required:true,
+        trim:true,
+    },
+
+    district:{
+        type:String,
+        lowercase:true,
+        required:true,
+        trim:true,
+    },
+
+    state:{
+        type:String,
+        lowercase:true,
+        required:true,
+        trim:true,
+    },
+
+    pincode:{
+        type:Number,
+        minLength:6,
+        maxLength:6,
+        required:true 
+    },
+
+
+    password: {
+        type:String,
+        required:true
+    },
+
+},{timestamps:true})
+
+const Organisation = mongoose.model("Organisation",organisationSchema);
+module.exports = Organisation
